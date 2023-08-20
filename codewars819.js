9331d53be0809f9b1bac9e80dc6066444f457a87
tdthevd <theodore.a.daly@gmail.com>
2023-08-19 21:01:22 -0400

    codewars819

---
+++ codewars819.js
@@ -0,0 +1,12 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+    return str.split('').map(e => e+e).join('')
+    }
+
+    const doubleChar = (str) => str.split("").map(c => c + c).join("");
