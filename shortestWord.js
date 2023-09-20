f98c7dc78c92ae67161d67473de3ae5c6acf4259
tdthevd <theodore.a.daly@gmail.com>
2023-09-20 14:04:42 -0400

    using sort method for finding length of shortest string element

---
+++ shortestWord.js
@@ -0,0 +1,14 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    // Step 1: Split the input string into an array of words
+    s = s.split(' ')
+    
+    // Step 2: Sort the array based on the length of the words (ascending order)
+    s = s.sort((a, b) => a.length - b.length)
+    
+    // Step 3: Return the length of the shortest word (which is the first element after sorting)
+    return s[0].length
+  }
