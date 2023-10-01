8f3416177b1a5e6cd70f29f6a0a09633b579dfde
tdthevd <theodore.a.daly@gmail.com>
2023-10-01 16:11:27 -0400

    boolToString

---
+++ boolToString.js
@@ -0,0 +1,19 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    if(b === true){
+      return 'true'
+    }else{
+      return 'false'
+    }
+  }
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
+
+function booleanToString(b){
+    return b.toString();
+  }
