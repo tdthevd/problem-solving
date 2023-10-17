225c030b1fe9a932f7e5bb923283f54879e9497c
tdthevd <theodore.a.daly@gmail.com>
2023-10-17 16:28:44 -0400

    create a class ghost and assign a random color

---
+++ ghostClass.js
@@ -0,0 +1,16 @@
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+class Ghost {
+    constructor(){
+      this.colors = ['white', 'yellow', 'purple', 'red'];
+      let randomIndex = Math.floor(Math.random() * this.colors.length);
+      this.color = this.colors[randomIndex];
+    }
+  }
