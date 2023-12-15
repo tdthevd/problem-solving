2ff5d544d046c48c0c4790955573cfe1dd8310e8
tdthevd <theodore.a.daly@gmail.com>
2023-12-15 18:50:15 -0500

    is every number below limit

---
+++ smallEnough.js
@@ -0,0 +1,13 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+    let count = 0
+    for(const i of a){
+      if(i<=limit){
+        count++
+      }
+    }
+    return count === a.length
+  }
