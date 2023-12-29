6081b66df09e50117d32e2dd61188a91dd0077e0
tdthevd <theodore.a.daly@gmail.com>
2023-12-28 19:50:00 -0500

    kyu7 & 8

---
+++ square.js
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+function square(a){
+    return a*a
+  }
+  const square = (n) => n * n;
+  function square(num){
+    var num = Math.pow(num, 2);
+    return num;
+  };
