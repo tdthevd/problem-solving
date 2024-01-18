834ded16f6a7ea92f3c31707d7373eacf776352c
tdthevd <theodore.a.daly@gmail.com>
2024-01-18 12:45:44 -0500

    does the array include input

--- arrows.js
+++ arrows.js
@@ -1,12 +1,5 @@
-// Check your arrows
-// You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
-// You need to verify that you have some good ones left, in order to prepare for battle:
-
-// anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
-// If an arrow in the quiver does not have a damaged status, it means it's new.
-
-// The expected result is a boolean, indicating whether you have any good arrows left.
-
-function anyArrows(arrows){
+// Function to check if there are any good arrows in the quiver
+function anyArrows(arrows) {
+    // Use the some method to check if there is at least one arrow without damage (damaged: false or not present)
     return arrows.some(arrow => !arrow.damaged);
-    }
+  }
